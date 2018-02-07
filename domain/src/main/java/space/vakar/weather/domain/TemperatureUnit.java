6bb3ca7e57be535dbb508add54cd3fb4af741c61
Leonid <leonid.vakar@gmail.com>
2018-02-07 09:07:10 +0200

    Clean unused

--- domain/src/main/java/space/vakar/weather/domain/TemperatureUnit.java
+++ domain/src/main/java/space/vakar/weather/domain/TemperatureUnit.java
@@ -1,19 +1,8 @@
 package space.vakar.weather.domain;
 
-public enum TemperatureUnit {
-	
-	KELVIN("kelvin"), 
-	CELSIUS("celsius"), 
-	FAHRENHEIT("fahrenheit"), 
-	UNIT_UNKNOWN("unit_unknown");
-
-	private final String openWeatherName;
-
-	TemperatureUnit(String openWeatherName) {
-		this.openWeatherName = openWeatherName;
-	}
-
-	public String openWeatherName() {
-		return openWeatherName;
-	}
+public enum TemperatureUnit {	
+	KELVIN, 
+	CELSIUS, 
+	FAHRENHEIT, 
+	UNIT_UNKNOWN;
 }
