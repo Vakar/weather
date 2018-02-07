9010e075f74b311e63390576b6a8310f268b2610
Leonid Vakar <leonid.vakar@gmail.com>
2018-02-07 12:15:03 +0200

    Clean code

--- open-weather/src/test/java/space/vakar/weather/openweather/OpenWeatherParserTest.java
+++ open-weather/src/test/java/space/vakar/weather/openweather/OpenWeatherParserTest.java
@@ -37,23 +37,31 @@ public class OpenWeatherParserTest {
 	}
 	
 	private CurrentWeather getExpectedWeather() {
-		CurrentWeatherBuilder cwb = new CurrentWeatherBuilder();
-		LocalDateTime rise = LocalDateTime.parse("2018-01-31T11:42:29");
-		LocalDateTime set = LocalDateTime.parse("2018-01-31T21:23:30");
-		Sun sun = new Sun(rise, set);
-		Coordinates coordinates = new Coordinates(-64.8, 46.1);
-		cwb.city(new City(6076211, "Moncton", "CA", sun, coordinates));
+		CurrentWeatherBuilder cwb = new CurrentWeatherBuilder();		
+		cwb.city(getCity());
 		cwb.temperature(new Temperature(261.15, 261.15, 261.15, TemperatureUnit.KELVIN));
 		cwb.humidity(new Humidity(85, "%"));
-		cwb.pressure(new Pressure(1002, "hPa"));
-		WindSpeed speed = new WindSpeed(7.7, "Moderate breeze");
-		WindDirection direction = new WindDirection(290, "WNW", "West-northwest");
-		cwb.wind(new Wind(speed, direction));
+		cwb.pressure(new Pressure(1002, "hPa"));		
+		cwb.wind(getWind());
 		cwb.clouds(new Clouds(90, "overcast clouds"));
 		cwb.visibility(new Visibility(4828));
 		cwb.precipitation(new Precipitation(13.4, "snow"));
 		cwb.weather(new Weather(600, "light snow", "13n"));
 		cwb.lastupdate(new LastUpdate(LocalDateTime.parse("2018-01-31T08:27:00")));
 		return cwb.build();
+	}
+	
+	private City getCity() {
+		LocalDateTime rise = LocalDateTime.parse("2018-01-31T11:42:29");
+		LocalDateTime set = LocalDateTime.parse("2018-01-31T21:23:30");
+		Sun sun = new Sun(rise, set);
+		Coordinates coordinates = new Coordinates(-64.8, 46.1);
+		return new City(6076211, "Moncton", "CA", sun, coordinates);
+	}
+	
+	private Wind getWind() {
+		WindSpeed speed = new WindSpeed(7.7, "Moderate breeze");
+		WindDirection direction = new WindDirection(290, "WNW", "West-northwest");
+		return new Wind(speed, direction);
 	}
 }
