bb3b2d44b23e436ac1219cf51ba5808a5388cd7b
Leonid Vakar <leonid.vakar@gmail.com>
2018-02-11 20:53:23 +0200

    Make WeatherContainer interface more straitforwatd

--- weather-service/src/main/java/space/vakar/weather/service/api/WeatherContainer.java
+++ weather-service/src/main/java/space/vakar/weather/service/api/WeatherContainer.java
@@ -2,9 +2,9 @@ package space.vakar.weather.service.api;
 
 import space.vakar.weather.domain.model.Weather;
 
-public interface WeatherContainer {
-	public void update(Weather weather);
-	public Weather weather(String cityName);
+public interface WeatherContainer {	
+	public void push(Weather weather);
+	public Weather pull(String cityName);
 	public boolean isExist(String citName);
-	public void put(Weather weather);
+	public boolean isFresh(String cityName);
 }
