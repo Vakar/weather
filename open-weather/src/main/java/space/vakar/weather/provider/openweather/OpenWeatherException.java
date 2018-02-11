0c94b58fb6a41d7704c76aee745065acc4b0e4b3
Leonid Vakar <leonid.vakar@gmail.com>
2018-02-11 20:10:17 +0200

    Add OpenWeatherException for open-weather layer

---
+++ open-weather/src/main/java/space/vakar/weather/provider/openweather/OpenWeatherException.java
@@ -0,0 +1,9 @@
+package space.vakar.weather.provider.openweather;
+
+public class OpenWeatherException extends RuntimeException {
+	
+	public OpenWeatherException(String message) {
+		super(message);
+	}
+
+}
