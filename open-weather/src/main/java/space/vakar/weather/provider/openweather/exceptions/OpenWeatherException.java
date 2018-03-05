33bd3939503b714e073d27007a0fb180de0e0f48
Leonid <leonid.vakar@gmail.com>
2018-03-05 18:32:43 +0200

    Minor fix

--- open-weather/src/main/java/space/vakar/weather/provider/openweather/exceptions/OpenWeatherException.java
+++
@@ -1,7 +0,0 @@
-package space.vakar.weather.provider.openweather.exceptions;
-
-public class OpenWeatherException extends RuntimeException {
-  public OpenWeatherException(String message, Throwable e) {
-    super(message, e);
-  }
-}
--- open-weather/src/main/java/space/vakar/weather/provider/openweather/exceptions/OpenWeatherProviderException.java
+++
@@ -1,9 +0,0 @@
-package space.vakar.weather.provider.openweather.exceptions;
-
-public class OpenWeatherProviderException extends Exception {
-
-  public OpenWeatherProviderException(String message, Throwable e) {
-    super(message, e);
-  }
-
-}
