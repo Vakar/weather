1241ac813cd6778bbd233d135b305843bbef9d94
Leonid Vakar <leonid.vakar@gmail.com>
2019-01-26 18:25:58 +0200

    Remove persistence module

--- persistence/persistence-impl/src/main/java/space/vakar/weather/persistence/repository/WeatherRepository.java
+++
@@ -1,8 +0,0 @@
-package space.vakar.weather.persistence.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import space.vakar.weather.persistence.entities.weather.Weather;
-
-public interface WeatherRepository extends JpaRepository<Weather, Long> {
-
-}
