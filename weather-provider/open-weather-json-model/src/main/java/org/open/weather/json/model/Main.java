2028adcbe4418d1896feae3998b6cd1dd865c3df
Leonid <leonid.vakar@gmail.com>
2019-07-18 15:07:17 +0300

    Add jsom model module.

---
+++ weather-provider/open-weather-json-model/src/main/java/org/open/weather/json/model/Main.java
@@ -0,0 +1,120 @@
+
+package org.open.weather.json.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Main implements Serializable {
+
+	private final static long serialVersionUID = 6029719905243037484L;
+
+	@SerializedName("temp")
+	@Expose
+	private BigDecimal temp;
+	@SerializedName("pressure")
+	@Expose
+	private int pressure;
+	@SerializedName("humidity")
+	@Expose
+	private int humidity;
+	@SerializedName("temp_min")
+	@Expose
+	private BigDecimal tempMin;
+	@SerializedName("temp_max")
+	@Expose
+	private BigDecimal tempMax;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Main other = (Main) obj;
+		return humidity == other.humidity && pressure == other.pressure && Objects.equals(temp, other.temp)
+				&& Objects.equals(tempMax, other.tempMax) && Objects.equals(tempMin, other.tempMin);
+	}
+
+	public int getHumidity() {
+		return humidity;
+	}
+
+	public int getPressure() {
+		return pressure;
+	}
+
+	public BigDecimal getTemp() {
+		return temp;
+	}
+
+	public BigDecimal getTempMax() {
+		return tempMax;
+	}
+
+	public BigDecimal getTempMin() {
+		return tempMin;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(humidity, pressure, temp, tempMax, tempMin);
+	}
+
+	public void setHumidity(int humidity) {
+		this.humidity = humidity;
+	}
+
+	public void setPressure(int pressure) {
+		this.pressure = pressure;
+	}
+
+	public void setTemp(BigDecimal temp) {
+		this.temp = temp;
+	}
+
+	public void setTempMax(BigDecimal tempMax) {
+		this.tempMax = tempMax;
+	}
+
+	public void setTempMin(BigDecimal tempMin) {
+		this.tempMin = tempMin;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Main [temp=%s, pressure=%s, humidity=%s, tempMin=%s, tempMax=%s]", temp, pressure,
+				humidity, tempMin, tempMax);
+	}
+
+	public Main withHumidity(int humidity) {
+		this.humidity = humidity;
+		return this;
+	}
+
+	public Main withPressure(int pressure) {
+		this.pressure = pressure;
+		return this;
+	}
+
+	public Main withTemp(BigDecimal temp) {
+		this.temp = temp;
+		return this;
+	}
+
+	public Main withTempMax(BigDecimal tempMax) {
+		this.tempMax = tempMax;
+		return this;
+	}
+
+	public Main withTempMin(BigDecimal tempMin) {
+		this.tempMin = tempMin;
+		return this;
+	}
+
+}
---
+++ weather-provider/open-weather-json-model/src/main/java/org/open/weather/json/model/Sys.java
@@ -0,0 +1,136 @@
+
+package org.open.weather.json.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Sys implements Serializable {
+
+	private final static long serialVersionUID = -6633630464954791315L;
+
+	@SerializedName("type")
+	@Expose
+	private int type;
+	@SerializedName("id")
+	@Expose
+	private int id;
+	@SerializedName("message")
+	@Expose
+	private BigDecimal message;
+	@SerializedName("country")
+	@Expose
+	private String country;
+	@SerializedName("sunrise")
+	@Expose
+	private int sunrise;
+	@SerializedName("sunset")
+	@Expose
+	private int sunset;
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sys other = (Sys) obj;
+		return Objects.equals(country, other.country) && id == other.id && Objects.equals(message, other.message)
+				&& sunrise == other.sunrise && sunset == other.sunset && type == other.type;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public BigDecimal getMessage() {
+		return message;
+	}
+
+	public int getSunrise() {
+		return sunrise;
+	}
+
+	public int getSunset() {
+		return sunset;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(country, id, message, sunrise, sunset, type);
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setMessage(BigDecimal message) {
+		this.message = message;
+	}
+
+	public void setSunrise(int sunrise) {
+		this.sunrise = sunrise;
+	}
+
+	public void setSunset(int sunset) {
+		this.sunset = sunset;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Sys [type=%s, id=%s, message=%s, country=%s, sunrise=%s, sunset=%s]", type, id, message,
+				country, sunrise, sunset);
+	}
+
+	public Sys withCountry(String country) {
+		this.country = country;
+		return this;
+	}
+
+	public Sys withId(int id) {
+		this.id = id;
+		return this;
+	}
+
+	public Sys withMessage(BigDecimal message) {
+		this.message = message;
+		return this;
+	}
+
+	public Sys withSunrise(int sunrise) {
+		this.sunrise = sunrise;
+		return this;
+	}
+
+	public Sys withSunset(int sunset) {
+		this.sunset = sunset;
+		return this;
+	}
+
+	public Sys withType(int type) {
+		this.type = type;
+		return this;
+	}
+
+}
