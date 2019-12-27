e651341953c8677567d5ddf5730e6707f8f0cb40
Leonid <leonid.vakar@gmail.com>
2019-12-27 18:52:25 +0200

    feat (open-weather-persistence): sort city list output by input string and limit output to 5 cities

--- weather-provider/open-weather-persistence/src/main/java/space/vakar/open/weather/persistence/CityServiceImp.java
+++ weather-provider/open-weather-persistence/src/main/java/space/vakar/open/weather/persistence/CityServiceImp.java
@@ -1,9 +1,14 @@
 package space.vakar.open.weather.persistence;
 
-import java.util.ArrayList;
-import java.util.List;
 import space.vakar.weather.domain.api.CityService;
 import space.vakar.weather.domain.model.City;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class CityServiceImp implements CityService {
 
@@ -11,18 +16,52 @@ public class CityServiceImp implements CityService {
 
   private static final String COLUMN_NAME = "NAME";
 
+  private static final int MAX_NUMBER_OF_CITIES = 5;
+
   @Override
   public List<City> getCitiesByName(String cityName) {
-    List<City> cities = new ArrayList<>();
+    List<City> cities;
     try {
       List<EntityCity> entities = daoCity.search(COLUMN_NAME, cityName);
-      for (EntityCity enitity : entities) {
-        cities.add(ModelConverter.getFrom(enitity));
-      }
+      cities = entities.stream().map(ModelConverter::getFrom).collect(Collectors.toList());
     } catch (Exception e) {
       throw new OpenWeatherPersistenceException(e.getMessage(), e);
     }
-    return cities;
+    return sortCities(cityName, cities).stream()
+        .limit(MAX_NUMBER_OF_CITIES)
+        .collect(Collectors.toList());
   }
 
+  List<City> sortCities(String inputString, List<City> cityList) {
+    List<City> sortedList = new ArrayList<>();
+    SortedMap<Integer, List<City>> citiesByRange = getCitiesByRange(inputString, cityList);
+    citiesByRange.forEach(
+        (range, cities) -> {
+          cities.sort(Comparator.comparing(City::getName));
+          sortedList.addAll(cities);
+        });
+    return sortedList;
+  }
+
+  SortedMap<Integer, List<City>> getCitiesByRange(String inputString, List<City> cityList) {
+    SortedMap<Integer, List<City>> citiesByRange = new TreeMap<>();
+    for (City city : cityList) {
+      int cityRange = rankCity(inputString, city);
+      if (!citiesByRange.containsKey(cityRange)) {
+        citiesByRange.put(cityRange, new ArrayList<>());
+      }
+      citiesByRange.get(cityRange).add(city);
+    }
+    return citiesByRange;
+  }
+
+  int rankCity(String inputString, City city) {
+    String normalizedInputString = inputString.toUpperCase();
+    String normalizedCityName = city.getName().toUpperCase();
+    int range = normalizedCityName.indexOf(normalizedInputString) + 1;
+    if (normalizedCityName.equals(normalizedInputString)) {
+      range = 0;
+    }
+    return range;
+  }
 }
---
+++ weather-provider/open-weather-persistence/src/test/java/space/vakar/open/weather/persistence/CityServiceImpTest.java
@@ -0,0 +1,40 @@
+package space.vakar.open.weather.persistence;
+
+import org.junit.Test;
+import space.vakar.weather.domain.model.City;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class CityServiceImpTest {
+
+  private CityServiceImp service = new CityServiceImp();
+
+  private String inputString = "London";
+
+  private City city1 = new City(1,  inputString, "GB");
+  private City city2 = new City(2, inputString + "A", "GB");
+  private City city3 = new City(3, "A" + inputString, "GB");
+  private City city4 = new City(4, "AA" + inputString + "B", "GB");
+  private City city5 = new City(5, "AA" + "london", "GB");
+
+  @Test
+  public void sortCitiesTest() {
+    List<City> unsortedCityList = Arrays.asList(city4, city3, city5, city1, city2);
+    List<City> sortedCityList = service.sortCities(inputString, unsortedCityList);
+    assertThat(sortedCityList, contains(city1, city2, city3, city4, city5));
+  }
+
+  @Test
+  public void rankCityTest() {
+    assertEquals(0, service.rankCity(inputString, city1));
+    assertEquals(1, service.rankCity(inputString, city2));
+    assertEquals(2, service.rankCity(inputString, city3));
+    assertEquals(3, service.rankCity(inputString, city4));
+    assertEquals(3, service.rankCity(inputString, city5));
+  }
+}
