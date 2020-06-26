e29159eb96c7dc319fb3173988972791d12e7037
Leonid <leonid.vakar@gmail.com>
2020-06-26 14:03:53 +0300

    cleanup (weather-rest): CORS filter

--- weather-rest/src/main/java/space/vakar/weather/rest/filter/CorsResponseFilter.java
+++ weather-rest/src/main/java/space/vakar/weather/rest/filter/CorsResponseFilter.java
@@ -18,7 +18,7 @@ public class CorsResponseFilter implements ContainerResponseFilter {
       ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
     MultivaluedMap<String, Object> headers = responseContext.getHeaders();
     headers.add("Access-Control-Allow-Origin", "*");
-    headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-    headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Codingpedia");
+    headers.add("Access-Control-Allow-Methods", "GET");
+    headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
   }
 }
